15ef4978a6b4e01248ea9cd4993322bccac8343b
Oumniya <oumniamoutaouakil@gmail.com>
2026-08-01 10:07:25 +0100

    les particules un peu plus intenses

--- scripts.js
+++ scripts.js
@@ -430,13 +430,13 @@ if (typeof particlesJS !== 'undefined') {
         "number": { "value": 60, "density": { "enable": true, "value_area": 800 } },
         "color": { "value": "#0ea5e9" }, // Couleur Sky-500
         "shape": { "type": "circle" },
-        "opacity": { "value": 0.3, "random": false },
+        "opacity": { "value": 0.5, "random": false },
         "size": { "value": 3, "random": true },
         "line_linked": {
           "enable": true,
           "distance": 150,
           "color": "#0ea5e9",
-          "opacity": 0.2,
+          "opacity": 0.4,
           "width": 1
         },
         "move": {
